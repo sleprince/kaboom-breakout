ac64f85be89d171cd77e071e4e5d53f82db0339d
sleprince <steffanleprince@gmail.com>
2023-07-18 15:36:22 +0100

    Adding bas gameobjects, no movement yet.

--- kaboom/mygame/src/main.js
+++ kaboom/mygame/src/main.js
@@ -5,153 +5,204 @@ let JUMP_FORCE = 800;
 const SPEED = 480;
 
 // initialize context
-kaboom();
-
-// load assets
-loadSprite("bean", "sprites/bean.png");
-loadSprite("tree", "sprites/tree.png");
-loadSprite("bg", "sprites/bg.png");
-
-scene("game", () => {
-
-    // define gravity
-    setGravity(1600);
-
-    //add background
-    add([sprite("bg", { width: width(), height: height() })]);
-
-    // add a game object to screen
-    const player = add([
-        // list of components
-        sprite("bean"),
-        pos(80, 40),
-        area(),
-        body(),
-    ]);
-
-    // floor
-    add([
-        rect(width(), FLOOR_HEIGHT),
-        outline(4),
-        pos(0, height()),
-        anchor("botleft"),
-        area(),
-        body({ isStatic: true }),
-        color(127, 200, 255),
-    ]);
-
-    function jump() {
-        if (player.isGrounded()) {
-            JUMP_FORCE = 1000;
-            setGravity(1600);
-            player.jump(JUMP_FORCE);
-        }
-    }
-
-    // jump when user press space
-    onKeyPressRepeat("space", jump);
-    onKeyRelease("space", () => {
-        (setGravity(5600));
-    })
-
-    //or left clicks
-    onMousePress("left", jump);
-    onMouseRelease("left", () => {
-        (setGravity(5600));
-    })
-
-
-
-    function spawnTree() {
-
-        let randomHeight = rand(36, 100)
-
-        // add tree obj
-        /*let base = add([
-            rect(48, randomHeight),
-            area(),
-            outline(4),
-            pos(width(), height() - FLOOR_HEIGHT),
-            anchor("botleft"),
-            color(255, 180, 255),
-            move(LEFT, SPEED),
-            "tree",
-        ]);
-        */
-
-        add([
-            sprite("tree"),
-            pos(width() - 20, height() - FLOOR_HEIGHT),
-            scale(0.25, randomHeight / 1000),
-            area(),
-            anchor("botleft"),
-            move(LEFT, SPEED),
-            "tree",
-        ]);
-
-        // wait a random amount of time to spawn next tree
-        wait(rand(0.5, 1.5), spawnTree);
-
-    }
-
-    // start spawning trees
-    spawnTree();
-
-    // lose if player collides with any game obj with tag "tree"
-    player.onCollide("tree", () => {
-        // go to "lose" scene and pass the score
-        go("lose", score);
-        burp();
-        addKaboom(player.pos);
-    });
-
-    // keep track of score
-    let score = 0;
-
-    const scoreLabel = add([
-        text(score),
-        pos(24, 24),
-    ]);
-
-    // increment score every frame
-    onUpdate(() => {
-        score++;
-        scoreLabel.text = score;
-    });
-
+kaboom({
+    width: 768,
+    height: 360,
+    background: [0, 0, 0],
 });
 
-scene("lose", (score) => {
-
-    //display player character sprite
-    add([
-        sprite("bean"),
-        pos(width() / 2, height() / 2 - 80),
-        scale(2),
-        anchor("center"),
-
-    ]);
-
-    //display game over text
-    add([
-        text("GAME OVER"),
-        pos(width() / 2, height() / 2 + 15),
-        scale(2),
-        anchor("center"),
-    ]);
-
-    // display score
-    add([
-        text(score),
-        pos(width() / 2, height() / 2 + 80),
-        scale(2),
-        anchor("center"),
-    ]);
-
-    // go back to game with space is pressed
-    onKeyPress("space", () => go("game"));
-    onClick(() => go("game"));
-
+//load all sprites
+loadSpriteAtlas("sprites/breakout_pieces.png", {
+    blocka: {
+        x: 8,
+        y: 8,
+        width: 32,
+        height: 16,
+    },
+    blockb: {
+        x: 8,
+        y: 28,
+        width: 32,
+        height: 16,
+    },
+    blockc: {
+        x: 8,
+        y: 48,
+        width: 32,
+        height: 16,
+    },
+    blockd: {
+        x: 8,
+        y: 68,
+        width: 32,
+        height: 16,
+    },
+    paddle: {
+        x: 8,
+        y: 152,
+        width: 64,
+        height: 16,
+    },
+    ball: {
+        x: 48,
+        y: 136,
+        width: 8,
+        height: 8,
+    },
+    heart: {
+        x: 120,
+        y: 136,
+        width: 8,
+        height: 8,
+    },
 });
 
-go("game");
+//add custom font
+loadFont("breakout", "sprites/breakout_font.png", 6, 8, {
+    chars: "ABCDEFGHIJKLMNOPQRSTUVWXYZ  0123456789:!'",
+});
+
+//add sounds
+loadSound("blockbreak", "sounds/Explosion5.ogg");
+loadSound("paddlehit", "sounds/Powerup20.ogg");
+loadSound("powerup", "sounds/Powerup2.ogg");
+loadSound("ArcadeOddities", "sounds/Arcade-Oddities.mp3");
+
+//ascii create levels
+const LEVELS = [
+    [
+        "                        ",
+        "                        ",
+        "dddddddddddddddddddddddd",
+        "cccccccccccccccccccccccc",
+        "bbbbbbbbbbbbbbbbbbbbbbbb",
+        "aaaaaaaaaaaaaaaaaaaaaaaa",
+        "                        ",
+        "                        ",
+        "                        ",
+        "            .           ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "            @           ",
+    ],
+    [
+        " aaaaaaaaaaaaaaaaaaaaaa ",
+        " a                    a ",
+        " a  bbbbbbbbbbbbbbbbb a ",
+        " a  b               b a ",
+        " a  b    ccccccc    b a ",
+        " a  b  ccdddddddcc  b a ",
+        " a  b    ccccccc    b a ",
+        " a  b               b a ",
+        " a  bbbbbbbbbbbbbbbbb a ",
+        " a                    a ",
+        " aaaaaaaaaaaaaaaaaaaaaa ",
+        "                        ",
+        "            .           ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "                        ",
+        "            @           ",
+    ],
+];
+
+//json levelopt defines all gameobjects
+const LEVELOPT = {
+    width: 32,
+    height: 16,
+    a: () => [
+        // block
+        sprite("blocka"),
+        area(),
+        "block",
+        "bouncy",
+        {
+            points: 1,
+        },
+    ],
+    b: () => [
+        // block
+        sprite("blockb"),
+        area(),
+        "block",
+        "bouncy",
+        {
+            points: 2,
+        },
+    ],
+    c: () => [
+        // block
+        sprite("blockc"),
+        area(),
+        "block",
+        "bouncy",
+        {
+            points: 4,
+        },
+    ],
+    d: () => [
+        // block
+        sprite("blockd"),
+        area(),
+        "block",
+        "bouncy",
+        {
+            points: 8,
+        },
+    ],
+    "@": () => [
+        // paddle
+        sprite("paddle"),
+        area(),
+        origin("center"),
+        "paddle",
+        "bouncy",
+        {
+            speed: 400,
+        },
+    ],
+    ".": () => [
+        // ball
+        sprite("ball"),
+        color(WHITE),
+        area(),
+        origin("center"),
+        "ball",
+        {
+            hspeed: 100,
+            vspeed: 50,
+        },
+    ],
+};
+
+scene("game", ({ levelIndex, score, lives }) => {
+    addLevel(LEVELS[levelIndex], LEVELOPT);
+});
+
+// start game on first level
+function start() {
+    go("game", {
+        levelIndex: 0,
+        score: 0,
+        lives: 3,
+    });
+}
+
+start();
+
